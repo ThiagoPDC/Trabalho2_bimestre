d23b72b6f2aeef29060357a063ec3448e87344f8
ThiagoPDC <thiagopavandallacosta@gmail.com>
2023-06-15 14:20:31 -0300

    Trabalho2 bimestre

---
+++ ThiagoPDC_PedroHB_LiviaD/src/thiagopdc_pedrohb_liviad/ThiagoPDC_PedroHB_LiviaD.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package thiagopdc_pedrohb_liviad;
+
+/**
+ *
+ * @author pdcth
+ */
+public class ThiagoPDC_PedroHB_LiviaD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
